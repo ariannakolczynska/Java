f405837f91e1abf91b12a1670128d716cebfc513
AleksandraKolczynska <54152507+AleksandraKolczynska@users.noreply.github.com>
2019-08-28 17:00:11 +0200

    Update cat.java

--- example of implements and extends/cat.java
+++ example of implements and extends/cat.java
@@ -1,13 +1,14 @@
 
-public class cat extends organism implements cats{
+public class cat extends organism implements cats {
 
 	public cat(int sleppingTime, String kindOfFun, String gender, String name) {
 		super(sleppingTime, kindOfFun, gender, name);
-		
+
 	}
 
-	public String toString(){
-		return  "Name: " + name + " Gender: " + gender + " Kind of fun: " + kindOfFun + " Type of food: " + eat + " Sound: "+ sound + " Slepping time: " + sleppingTime + "h";
+	public String toString() {
+		return "Name: " + name + " Gender: " + gender + " Kind of fun: " + kindOfFun + " Type of food: " + eat
+				+ " Sound: " + sound + " Slepping time: " + sleppingTime + "h";
 	}
 
 }
