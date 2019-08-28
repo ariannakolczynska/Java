0a7583f24a1c4c5e70f5f1bf706baa82949ad292
AleksandraKolczynska <54152507+AleksandraKolczynska@users.noreply.github.com>
2019-08-28 16:47:52 +0200

    Update listmovie.java

--- wypożyczalnia płyt/listmovie.java
+++ wypożyczalnia płyt/listmovie.java
@@ -4,17 +4,19 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class listmovie {
-List<movie> Listm = new ArrayList();
-public void putListm(Object t){
-  Listm.add((movie) t);
-  
+	List<movie> Listm = new ArrayList();
+
+	public void putListm(Object t) {
+		Listm.add((movie) t);
+
+	}
+
+	public void view() {
+		Listm.stream().map(Object::toString).forEach(System.out::println);
+
+	}
+
+	public void sort() {
+		Collections.sort(Listm, new sortmovie());
+	}
 }
- public void view() {
-	 Listm.stream().map(Object::toString).forEach( System.out::println);
-	 
- }
- public void sort() {
-	 Collections.sort(Listm, new sortmovie());
- }
-}
- 
